862a78b7f6f192b8ce48e2f3abdd4150cf42fc1e
StrawberrySunset <strawberrysunset@protonmail.com>
2020-10-31 23:26:03 +0000

    Remove unused page.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { colors, GlobalStyling } from './theme'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import { Home, MotionGraphics, Photography, Error404 } from './pages'
+import { Home, MotionGraphics, Error404 } from './pages'
 import { Header, Footer, BackToTop } from './components'
 
 const SiteWrapper = styled.div`
@@ -16,14 +16,12 @@ export default function App () {
     return (
         <Router>
             <GlobalStyling/>
-            {/* <a id="top"></a> */}
             <SiteWrapper>
                 <Header/>
                 <BackToTop/>
                 <Switch>
                   <Route path="/" exact component={Home}/>
                   <Route path="/motiongraphics" component={MotionGraphics}/>
-                  <Route path="/photography" component={Photography}/> 
                   <Error404/>
               </Switch>
                 <Footer/>
